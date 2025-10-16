02e1a19132aed9f5d5b2e686503c26e9b47d9689
mohamed khalifa <mohamed.khaled.khalifa.99@gmail.com>
2025-10-16 15:47:17 +0300

    Update MotorClaimCommentController.php

--- app/Http/Controllers/MotorClaimCommentController.php
+++ app/Http/Controllers/MotorClaimCommentController.php
@@ -88,7 +88,7 @@ class MotorClaimCommentController extends Controller
                         'Accept' => '*/*',
                                 ]
                                     ]);
-                            $URI = 'https://digitalbondmena.com/insurancenotification/sendSingleNotificationClaim';
+                            $URI = 'https://api.cairohere.com/api/sendSingleNotificationClaim';
                             $body['titlemessage'] = 'New Comment!';
                             $body['textmessage'] = 'You have a new comment received. Tap to view and respond';
                             $body['artitlemessage'] = 'تعليق جديد!';
