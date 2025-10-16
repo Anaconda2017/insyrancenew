bf1c58ada9ae3c06d88acf086756cba9b833b883
mohamed khalifa <mohamed.khaled.khalifa.99@gmail.com>
2025-10-16 15:45:39 +0300

    Update MedicalClaimController.php

--- app/Http/Controllers/MedicalClaimController.php
+++ app/Http/Controllers/MedicalClaimController.php
@@ -144,7 +144,7 @@ class MedicalClaimController extends Controller
                         'Accept' => '*/*',
                                 ]
                                     ]);
-                            $URI = 'https://digitalbondmena.com/insurancenotification/sendSingleNotificationClaim';
+                            $URI = 'https://api.cairohere.com/api/sendSingleNotificationClaim';
                             $body['titlemessage'] = 'Update Alert!';
                             $body['titlemessage'] = 'Update Alert!';
                             $body['textmessage'] = 'Your status has been updated.';
