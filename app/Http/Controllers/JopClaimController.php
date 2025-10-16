6d92fc7f430e0dc7139ba4109bc15e19cf779837
mohamed khalifa <mohamed.khaled.khalifa.99@gmail.com>
2025-10-16 15:43:03 +0300

    Update JopClaimController.php

--- app/Http/Controllers/JopClaimController.php
+++ app/Http/Controllers/JopClaimController.php
@@ -162,7 +162,7 @@ class JopClaimController extends Controller
                         'Accept' => '*/*',
                                 ]
                                     ]);
-                            $URI = 'https://digitalbondmena.com/insurancenotification/sendSingleNotificationClaim';
+                            $URI = 'https://api.cairohere.com/api/sendSingleNotificationClaim';
                             $body['titlemessage'] = 'Update Alert!';
                             $body['textmessage'] = 'Your status has been updated.';
                             $body['artitlemessage'] = 'تنبيه تحديث!';
