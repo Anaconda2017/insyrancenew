ee33af0b17e293dce9c3e5224a2e9926999355e7
mohamed khalifa <mohamed.khaled.khalifa.99@gmail.com>
2025-10-16 15:10:24 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,6 +36,22 @@ class AppServiceProvider extends ServiceProvider
             Artisan::call('config:clear');
             Artisan::call('route:clear');
             Artisan::call('view:clear');
+
+         \Illuminate\Support\Facades\Response::macro('noCache', function ($response) {
+            return $response->header('Cache-Control', 'no-cache, no-store, must-revalidate')
+                            ->header('Pragma', 'no-cache')
+                            ->header('Expires', '0');
+        });
+
+        // إضافة global hook على كل Response
+        \Illuminate\Support\Facades\Event::listen(
+            \Illuminate\Routing\Events\ResponseSending::class,
+            function ($event) {
+                $event->response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
+                $event->response->headers->set('Pragma', 'no-cache');
+                $event->response->headers->set('Expires', '0');
+            }
+        );
         
         Schema::defaultStringLength(191);
     
